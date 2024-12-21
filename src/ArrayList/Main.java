9bc279e912e69a9ed18161eed7e53c3c7bc615a2
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-12-21 11:38:31 +0530

    gjhdlk

--- src/ArrayList/Main.java
+++ src/ArrayList/Main.java
@@ -1,14 +1,16 @@
 package ArrayList;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> list=new ArrayList<>(1000);
-       list.add(1);
+      //  ArrayList<Integer> list=new ArrayList<>(1000);
+   //    list.add(1);
 //        list.add(23);
 //        list.add(56);
-        System.out.println(list.size());
+     //   System.out.println(list.size());
 //        System.out.println(list.get(2));
 //        System.out.println(list.size());
 //        for(int i=0; i<list.size(); i++){
@@ -17,8 +19,17 @@ public class Main {
 //        for(int x: list){
 //            System.out.print(x);
 //        }
-        list.remove(2);
-        System.out.println(list.size());
-        System.out.println(list.get(1));
+       // list.remove(2);
+        //System.out.println(list.size());
+       // System.out.println(list.get(1));
+        List<String> list=new ArrayList<>();
+        System.out.println(list.getClass().getName());
+        List<String> list1= Arrays.asList("Monday","Tuesday");
+        System.out.println(list1.getClass().getName());
+        list1.add("Wednesday");
+        String[] array={"Apple","Banana","Mango"};
+        List<String> list2=Arrays.asList(array);
+
+
     }
 }
