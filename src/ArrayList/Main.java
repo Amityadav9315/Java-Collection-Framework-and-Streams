677b1f495781b4c210c84d012830137e42e1a8b4
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-12-21 11:07:57 +0530

    Java collection Framework

--- src/ArrayList/Main.java
+++ src/ArrayList/Main.java
@@ -1,4 +1,17 @@
 package ArrayList;
 
+import java.util.ArrayList;
+
 public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(1);
+        list.add(23);
+        list.add(56);
+        System.out.println(list.get(2));
+        System.out.println(list.size());
+        for(int i=0; i<list.size(); i++){
+            System.out.println(list.get(i));
+        }
+    }
 }
