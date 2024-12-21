3f3c7c223d7ba5f59868a50c02762f122295e941
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-12-21 11:25:28 +0530

    collection framework

--- src/ArrayList/Main.java
+++ src/ArrayList/Main.java
@@ -4,10 +4,11 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> list=new ArrayList<>();
-        list.add(1);
-        list.add(23);
-        list.add(56);
+        ArrayList<Integer> list=new ArrayList<>(1000);
+       list.add(1);
+//        list.add(23);
+//        list.add(56);
+        System.out.println(list.size());
 //        System.out.println(list.get(2));
 //        System.out.println(list.size());
 //        for(int i=0; i<list.size(); i++){
