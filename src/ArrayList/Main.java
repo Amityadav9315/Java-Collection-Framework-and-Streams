008cda3f69ec0f494cf2b50a15cc86fa911e5278
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-12-21 11:13:10 +0530

    collectionFramework

--- src/ArrayList/Main.java
+++ src/ArrayList/Main.java
@@ -8,10 +8,16 @@ public class Main {
         list.add(1);
         list.add(23);
         list.add(56);
-        System.out.println(list.get(2));
+//        System.out.println(list.get(2));
+//        System.out.println(list.size());
+//        for(int i=0; i<list.size(); i++){
+//            System.out.println(list.get(i));
+//        }
+//        for(int x: list){
+//            System.out.print(x);
+//        }
+        list.remove(2);
         System.out.println(list.size());
-        for(int i=0; i<list.size(); i++){
-            System.out.println(list.get(i));
-        }
+        System.out.println(list.get(1));
     }
 }
