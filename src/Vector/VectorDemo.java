dd71d6daba7eb48b3cda61e9edf9992272ba1431
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-12-21 12:44:16 +0530

    java collection

--- src/Vector/VectorDemo.java
+++ src/Vector/VectorDemo.java
@@ -4,13 +4,15 @@ import java.util.Vector;
 
 public class VectorDemo {
     public static void main(String[] args) {
-        Vector<Integer> vector=new Vector<>(5);
+        Vector<Integer> vector=new Vector<>(5,3);
         vector.add(1);
         vector.add(1);
         vector.add(1);
         vector.add(1);
         vector.add(1);
-        System.out.println(vector);
+        vector.add(1);
+
+        System.out.println(vector.capacity());
 
 
     }
