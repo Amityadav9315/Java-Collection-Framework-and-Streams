17cb7012f7adc3dd2a0eaac45f86ea403f8e0dfa
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-12-22 22:04:38 +0530

    hashMap

--- src/Map/HashCodeAndEqualsMethod.java
+++ src/Map/HashCodeAndEqualsMethod.java
@@ -1,6 +1,20 @@
 package Map;
 
+import java.time.Period;
+import java.util.HashMap;
+
 public class HashCodeAndEqualsMethod {
+    public static void main(String[] args) {
+        HashMap<person,String> map=new HashMap<>();
+        person p1=new person("Alice",1);
+        person p2=new person("Bop",2);
+        person p3=new person("Alice",1);
+
+
+        map.put(p1,"Engineer");
+        map.put(p2,"Desiginer");
+        map.put(p3,"Manager");
+    }
 
 }
 
