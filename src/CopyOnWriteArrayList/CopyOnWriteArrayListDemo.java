f2fd000e78ddcc89b2d43f51c14ed5a2c84ce71b
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-12-22 22:28:21 +0530

    collection framekwork

--- src/CopyOnWriteArrayList/CopyOnWriteArrayListDemo.java
+++ src/CopyOnWriteArrayList/CopyOnWriteArrayListDemo.java
@@ -2,24 +2,26 @@ package CopyOnWriteArrayList;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class CopyOnWriteArrayListDemo {
     public static void main(String[] args) {
 
-        List<String> shoppinglist=new ArrayList<>();
+        List<String> shoppinglist=new CopyOnWriteArrayList<>();
         shoppinglist.add("Milk");
         shoppinglist.add("egg");
-        shoppinglist.add("Butter");
         shoppinglist.add("Bread");
+       // shoppinglist.add("Bread");
         System.out.println("initial shopping list"+ shoppinglist);
         for(String item:shoppinglist){
             System.out.println(item);
             //try to modify shopping list while reading
-            if(item.equals("Egg")){
+            if(item.equals("egg")){
                 shoppinglist.add("Jam");
-                System.out.println("Add cutter while reading");
+                System.out.println("Add jam while reading");
             }
         }
+        System.out.println("Updated shoppinglist"+ shoppinglist);
 
 
     }
