54bce018aa23ffa369c8c235d4d26e04f673d73f
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-12-22 21:48:09 +0530

    hashmap

---
+++ src/Map/HashCodeAndEqualsMethod.java
@@ -0,0 +1,24 @@
+package Map;
+
+public class HashCodeAndEqualsMethod {
+
+}
+
+class person{
+    private String  name;
+    private int id;
+
+
+    public person(String name,int id){
+        this.name=name;
+        this.id=id;
+
+
+    }
+    public String getName(){
+        return name;
+    }
+    public int getId(){
+        return id;
+    }
+}
