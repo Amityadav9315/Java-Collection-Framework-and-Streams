7da8dba1db9b994a4a2465c6ffa1783f6c5c1aa1
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-12-22 21:53:22 +0530

    HashMap

---
+++ src/Map/HashMapDemo.java
@@ -0,0 +1,13 @@
+package Map;
+import java.util.HashMap;
+
+public class HashMapDemo {
+    public static void main(String[] args) {
+        HashMap<Integer,String> map=new HashMap<>();
+        map.put(1,"Akshita");
+        map.put(2,"Neha");
+        map.put(3,"Shubham");
+
+        System.out.println(map);
+    }
+}
