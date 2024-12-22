5a6d6d7da9203d25c40f6e5d6839adfc75057826
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-12-22 22:17:41 +0530

    framework

--- src/CopyOnWriteArrayList/CopyOnWriteArrayListDemo.java
+++ src/CopyOnWriteArrayList/CopyOnWriteArrayListDemo.java
@@ -1,7 +1,15 @@
 package CopyOnWriteArrayList;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CopyOnWriteArrayListDemo {
     public static void main(String[] args) {
+
+        List<String> shoppinglist=new ArrayList<>();
+        shoppinglist.add("Milk");
+        shoppinglist.add("Butter");
+
 
     }
 }
