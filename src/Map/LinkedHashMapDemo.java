4a01862c2737c5cd726bc81bd8d9bd20f02304f5
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-12-24 11:16:58 +0530

    LinkedHashMap

---
+++ src/Map/LinkedHashMapDemo.java
@@ -0,0 +1,11 @@
+package Map;
+
+import java.util.LinkedHashMap;
+
+public class LinkedHashMapDemo {
+    public static void main(String[] args) {
+        LinkedHashMap<String ,Integer> linkedHashMap=new LinkedHashMap<>();
+
+
+    }
+}
