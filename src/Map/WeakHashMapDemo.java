2dd79f746574c60d94e146c6cacae3db8272a7e3
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-12-24 10:32:11 +0530

    weakhashmap

--- src/Map/WeakHashMapDemo.java
+++ src/Map/WeakHashMapDemo.java
@@ -1,11 +1,16 @@
 package Map;
 
+import java.util.WeakHashMap;
+
 public class WeakHashMapDemo {
 
 
 
 
     public static void main(String[] args) {
+        WeakHashMap<String,Image> imageCache=new WeakHashMap<>();
+        imageCache.put("img1",new Image("Image 1"));
+        imageCache.put("img2",new Image("Image2"));
 
 
     }
