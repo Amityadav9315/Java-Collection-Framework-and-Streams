aeede8a5bffc3b528de342b810743c63100e2461
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-12-24 11:06:27 +0530

    linked Hashmap

--- src/Map/LRUCache.java
+++ src/Map/LRUCache.java
@@ -1,6 +1,7 @@
 package Map;
 
 import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class LRUCache<K,V> extends LinkedHashMap<K,V> {
 
@@ -8,7 +9,12 @@ public class LRUCache<K,V> extends LinkedHashMap<K,V> {
 
         private int capacity;
 
-        public LRUCache(int capacity){
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+        return size()>capacity;
+    }
+
+    public LRUCache(int capacity){
             super(capacity,0.75f,true);
             this.capacity=capacity;
 
