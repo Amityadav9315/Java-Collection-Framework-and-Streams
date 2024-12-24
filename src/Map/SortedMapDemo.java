68a869d09eab02c54382b8330c178d3520b480f9
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-12-24 20:08:28 +0530

    navigablemap

--- src/Map/SortedMapDemo.java
+++ src/Map/SortedMapDemo.java
@@ -4,15 +4,22 @@ import java.util.*;
 
 public class SortedMapDemo {
     public static void main(String[] args) {
-        SortedMap<String, Integer>map = new TreeMap<>();
+       // SortedMap<String, Integer>map = new TreeMap<>();
 
-        map.put("Mohit",91);
-        map.put("Amit",34);
-        map.put("Shayam",76);
-        System.out.println(map);
+//        map.put("Mohit",91);
+//        map.put("Amit",34);
+//        map.put("Shayam",76);
+//        System.out.println(map);
+//
+//        System.out.println(map.firstKey());
+//        System.out.println(map.lastKey());
 
-        System.out.println(map.firstKey());
-        System.out.println(map.lastKey());
+      SortedMap<Integer,String> navgableMap=new TreeMap<>();
+      navgableMap.put(1,"One");
+      navgableMap.put(2,"Two");
+      navgableMap.put(5,"Five");
+        System.out.println(navgableMap);
+
 
 
     }
