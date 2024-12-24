cb91db33dc34ff45fecb6fecc9bef3836bbe452d
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-12-24 20:47:26 +0530

    concurrentHashMap

---
+++ src/Map/ConcurrentHashMapDemo.java
@@ -0,0 +1,12 @@
+package Map;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConcurrentHashMapDemo {
+    public static void main(String[] args) {
+        ConcurrentHashMap<String,Integer> map= new ConcurrentHashMap<>();
+        //  Java 7 segment based locking
+        //
+
+    }
+}
