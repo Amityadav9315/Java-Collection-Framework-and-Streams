20e6723eb15d17c7c59baff1db7ec5c30c0a68bb
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-12-24 10:30:47 +0530

    LinkedHashmap

--- src/Map/LinkedHashMapDemo.java
+++ src/Map/LinkedHashMapDemo.java
@@ -1,10 +1,19 @@
 package Map;
 
+import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class LinkedHashMapDemo {
     public static void main(String[] args) {
         LinkedHashMap<String ,Integer> linkedHashMap=new LinkedHashMap<>();
+        HashMap<String,Integer> hashMap=new HashMap<>();
+        hashMap.put("orange",10);
+        hashMap.put("Apple",20);
+        hashMap.put("Guava",13);
+        for(Map.Entry<String,Integer> entry : hashMap.entrySet()){
+            System.out.println(entry.getKey() + ": "+entry.getValue());
+        }
 
 
     }
