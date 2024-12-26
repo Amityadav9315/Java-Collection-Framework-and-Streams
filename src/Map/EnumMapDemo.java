0ced0e015cc18bd78b437ec714e4a2544299e7d1
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-12-26 11:02:36 +0530

    Enummap

--- src/Map/EnumMapDemo.java
+++ src/Map/EnumMapDemo.java
@@ -2,11 +2,13 @@ package Map;
 
 import java.time.DayOfWeek;
 import java.util.EnumMap;
-import java.util.HashMap;
+import java.util.Map;
 
 public class EnumMapDemo {
     public static void main(String[] args) {
-        Enum<Day,String> map=new EnumMap<>();  
+        Map<Day,String> map=new EnumMap<>(Day.class);
+        map.put(Day.TUESDAY,"Gym");
+        System.out.println(Day.TUESDAY.ordinal());
     }
 
     }
