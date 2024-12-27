e6ae0289931ab2655bf9e7afe1d657b37a840cb3
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-12-27 12:13:01 +0530

    wfdhlgf

--- src/Queue/BlockingQueueDemo.java
+++ src/Queue/BlockingQueueDemo.java
@@ -2,24 +2,40 @@ package Queue;
 
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
-   class Producer implements Runnable{
-       private  BlockingQueue<Integer> queue;
-       private int value=0;
+   class Producer implements Runnable {
+       private BlockingQueue<Integer> queue;
+       private int value = 0;
 
-       public Producer(BlockingQueue<Integer>queue){
-           this.queue=queue;
+       public Producer(BlockingQueue<Integer> queue) {
+           this.queue = queue;
 
        }
+
        @Override
-       public void run(){
+       public void run() {
+           while (true) {
+               try {
+
+                   Integer value = queue.take();
+
+                   System.out.println("Producer:" + value);
+                   queue.put(value);
+                   Thread.sleep(500);
+               } catch (Exception ignored) {
+                   Thread.currentThread().interrupt();
+                   System.out.println("Producer interrupted");
+
+               }
+
+           }
 
        }
 
+       public class BlockingQueueDemo {
+           public static void main(String[] args) {
+               BlockingQueue<Integer> blockingQueue = new ArrayBlockingQueue<>(5);
+
+
+           }
+       }
    }
-public class BlockingQueueDemo {
-    public static void main(String[] args) {
-        BlockingQueue<Integer> blockingQueue=new ArrayBlockingQueue<>(5);
-
-
-    }
-}
