ce55cb60cc91febe6fcdac36f81899cfa6618b9a
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-12-27 11:56:05 +0530

    Setmap

---
+++ src/Set/SetOverview.java
@@ -0,0 +1,19 @@
+package Set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetOverview {
+    public static void main(String[] args) {
+
+        Set<Integer>  set=new HashSet<>();
+        set.add(34);
+        set.add(65);
+        set.add(67);
+        set.add(89);
+        System.out.println(set);
+
+    }
+
+    }
+
