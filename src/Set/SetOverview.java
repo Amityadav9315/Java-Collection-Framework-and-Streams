773164e281b157d8718b1df8e697f1af8634bd82
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-12-27 12:00:59 +0530

    SetOverView

--- src/Set/SetOverview.java
+++ src/Set/SetOverview.java
@@ -3,6 +3,7 @@ package Set;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SetOverview {
     public static void main(String[] args) {
@@ -18,11 +19,20 @@ public class SetOverview {
 //
 
         //for as it is order
-        Set<Integer> set=new LinkedHashSet<>();
+//        Set<Integer> set=new LinkedHashSet<>();
+//        set.add(34);
+//        set.add(54);
+//        set.add(76);
+//        set.add(98);
+//        System.out.println(set);
+
+        //for ascending order
+        Set<Integer> set=new TreeSet<>();
+        set.add(45);
         set.add(34);
-        set.add(54);
-        set.add(76);
         set.add(98);
+        set.add(0);
+        set.add(23);
         System.out.println(set);
 
 
