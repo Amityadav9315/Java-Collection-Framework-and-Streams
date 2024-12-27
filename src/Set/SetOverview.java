11409ce544c0c6aabb4df93e137c0e621af3fa42
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-12-27 11:57:59 +0530

    LinkedHashSet

--- src/Set/SetOverview.java
+++ src/Set/SetOverview.java
@@ -1,6 +1,7 @@
 package Set;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class SetOverview {
@@ -8,10 +9,19 @@ public class SetOverview {
 
 
         //for random order
-        Set<Integer> set = new HashSet<>();
+//        Set<Integer> set = new HashSet<>();
+//        set.add(34);
+//        set.add(54);
+//        set.add(65);
+//        set.add(98);
+//        System.out.println(set);
+//
+
+        //for as it is order
+        Set<Integer> set=new LinkedHashSet<>();
         set.add(34);
         set.add(54);
-        set.add(65);
+        set.add(76);
         set.add(98);
         System.out.println(set);
 
